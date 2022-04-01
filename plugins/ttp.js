11fceaabae4712c919a55d2282474975a21d0ffe
ktdprjct <97012512+ktdprjct@users.noreply.github.com>
2022-04-01 14:01:15 +0800

    update ttp.js

--- plugins/ttp.js
+++ plugins/ttp.js
@@ -1,9 +1,9 @@
-const { sticker } = require('../lib/sticker')
+const { sticker, sticker5  } = require('../lib/sticker')
 const { MessageType } = require('@adiwajshing/baileys')
 
 let handler = async (m, { conn, text }) => {
   let teks = text ? text : m.quoted && m.quoted.text ? m.quoted.text : m.text
-  let stiker = await sticker(null, global.API('xteam', '/ttp', { file: '', text: teks }), global.packname, global.author)
+  let stiker = await sticker5(null, global.API('xteam', '/ttp', { file: '', text: teks }), global.packname, global.author)
   if (stiker) return conn.sendMessage(m.chat, stiker, MessageType.sticker, {
     quoted: m
   })
