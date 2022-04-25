9a1ed91210c04de715a306d880a2c75d8267d30b
ktdprjct <97012512+ktdprjct@users.noreply.github.com>
2022-04-26 07:23:30 +0800

    update play.js

--- plugins/play.js
+++ plugins/play.js
@@ -1,57 +1,45 @@
-let limit = 30
+const { servers, yta, ytv } = require('../lib/y2mate')
 let yts = require('yt-search')
 let fetch = require('node-fetch')
-const { servers, yta, ytv } = require('../lib/y2mate')
-let handler = async (m, { conn, command, text, isPrems, isOwner }) => {
-  if (!text) throw 'Cari apa?'
-  m.reply('_sedang diproses..._')
+let handler = async (m, { conn, command, text, usedPrefix }) => {
+  if (!text) throw `uhm.. cari apa?\n\ncontoh:\n${usedPrefix + command} alanwalker lily`
   let chat = global.db.data.chats[m.chat]
   let results = await yts(text)
-  let vid = results.all.find(video => video.seconds < 3600)
-  if (!vid) throw 'Video/Audio Tidak ditemukan'
+  let vid = results.all.find(video => video.seconds < 600)
+  if (!vid) throw 'Konten Tidak ditemukan/konten lebih dari 10 menit'
   let isVideo = /2$/.test(command)
   let yt = false
+  let yt2 = false
   let usedServer = servers[0]
   for (let i in servers) {
     let server = servers[i]
     try {
-      yt = await (isVideo ? ytv : yta)(vid.url, server)
+      yt = await yta(vid.url, server)
+      yt2 = await ytv(vid.url, server)
       usedServer = server
       break
-    } catch (e) {
+    }catch (e) {
       m.reply(`Server ${server} error!${servers.length >= i + 1 ? '' : '\nmencoba server lain...'}`)
     }
   }
-  if (yt === false) throw 'Semua server tidak bisa :/'
+  if (yt === false) throw 'semua server gagal'
+  if (yt2 === false) throw 'semua server gagal'
   let { dl_link, thumb, title, filesize, filesizeF } = yt
-  let isLimit = (isPrems || isOwner ? 99 : limit) * 1024 < filesize
-  conn.sendButtonLoc(m.chat, thumb, `
-➪ *Title:* ${title}
-➪ *Filesize:* ${filesizeF}
-➪ *Source:* ${vid.url}
-➪ *Link download:* ${await shortlink(dl_link)}
-➪ *Server y2mate:* ${usedServer}
-`.trim(), 'ktdprjct', 'Wait', 'biargambargadiklik', m)
-let _thumb = {}
-try { if (isVideo) _thumb = { thumbnail: await (await fetch(thumb)).buffer() } }
-catch (e) { }
-conn.sendFile(m.chat, dl_link, title + '.mp' + (3 + /2$/.test(command)), `
+  let ply = `━━━━━°❀❬ *PLAY* ❭❀°━━━━━
 *Title:* ${title}
-*Filesize:* ${filesizeF}
+*Audio:* ${filesizeF}
+*Video:* ${yt2.filesizeF}
+*Duration:* ${vid.timestamp}
 *Source:* ${vid.url}
-*Server y2mate:* ${usedServer}
-`.trim(), m, false,  {
-  ..._thumb,
-  asDocument: chat.useDocument
-})
+*Server y2mate:* ${usedServer}`
+  conn.send2ButtonLoc(m.chat, await (await fetch(thumb)).buffer(), ply,`ktdprjct`, `Audio 🎶`, `${usedPrefix}ytmp3 ${vid.url}`, `Video 📽️`, `${usedPrefix}ytmp4 ${vid.url}`, m)                                    
 }
 handler.help = ['play', 'play2'].map(v => v + ' <pencarian>')
 handler.tags = ['downloader']
 handler.command = /^play2?$/i
 
+handler.exp = 0
+handler.limit = true
+
 module.exports = handler
 
-async function shortlink(url) {
-isurl = /https?:\/\//.test(url)
-return isurl ? (await require('axios').get('https://tinyurl.com/api-create.php?url='+encodeURIComponent(url))).data : ''
-}
