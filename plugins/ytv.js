61e916b54ed0df101389f8d54ceba9c0e6f2250e
ktdprjct <97012512+ktdprjct@users.noreply.github.com>
2022-04-26 07:19:51 +0800

    update ytv.js

--- plugins/ytv.js
+++ plugins/ytv.js
@@ -1,17 +1,22 @@
 let limit = 30
 let fetch = require('node-fetch')
 const { servers, ytv } = require('../lib/y2mate')
-let handler = async (m, { conn, args, isPrems, isOwner }) => {
+let yts = require('yt-search')
+let handler = async (m, { conn, text, args, isPrems, isOwner }) => {
   if (!args || !args[0]) throw 'Uhm... urlnya mana?'
   let chat = global.db.data.chats[m.chat]
+  let results = await yts(text)
+  let vid = results.all.find(video => video.seconds < 300)
+  if (!vid) throw 'Konten Tidak ditemukan/konten lebih dari 5 menit'
   let server = (args[1] || servers[0]).toLowerCase()
   let { dl_link, thumb, title, filesize, filesizeF} = await ytv(args[0], servers.includes(server) ? server : servers[0])
   let isLimit = (isPrems || isOwner ? 99 : limit) * 1024 < filesize
-  conn.sendFile(m.chat, thumb, 'thumbnail.jpg', `
+  conn.sendButtonLoc(m.chat, thumb, `
 *Title:* ${title}
+*Duration:* ${vid.timestamp}
 *Filesize:* ${filesizeF}
-*${isLimit ? 'Pakai ': ''}Link:* ${dl_link}
-`.trim(), m)
+*${isLimit ? 'Pakai ': ''}Link:* ${await shortlink(dl_link)}
+`.trim(), 'ktdprjct', 'wait', 'wait', m)
   let _thumb = {}
   try { _thumb = { thumbnail: await (await fetch(thumb)).buffer() } }
   catch (e) { }
@@ -41,3 +46,7 @@ handler.limit = true
 
 module.exports = handler
 
+async function shortlink(url) {
+isurl = /https?:\/\//.test(url)
+return isurl ? (await require('axios').get('https://tinyurl.com/api-create.php?url='+encodeURIComponent(url))).data : ''
+}
