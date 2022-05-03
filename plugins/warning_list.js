4db88ae4a4d33e747f0b667fe76d06660ea022ca
ktdprjct <97012512+ktdprjct@users.noreply.github.com>
2022-05-03 23:18:35 +0800

    update warning_list.js

--- plugins/warning_list.js
+++ plugins/warning_list.js
@@ -1,13 +1,21 @@
-let handler = async (m, { conn }) => {
-      let mention = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted ? m.quoted.sender : m.sender
-      let warn = global.db.data.users[mention].warn
-      m.reply(`Kamu memiliki Total Warning : ${warn}`)
+let handler = async (m, { conn, isGroup }) => {
+  if (!m.isGroup) throw "Perintah ini hanya bisa digunakan di group!"
+  let warnLists = []
+  for (let users in global.db.data.warns[m.chat].users) {
+    let warnCount = global.db.data.warns[m.chat].users[users].count
+    let warnReason = global.db.data.warns[m.chat].users[users].reason
+    let str = `=> @${users.split("@")[0]}\n=> Warn count : ${warnCount}\n=> Reason : \n${warnReason.map((v, i) => `=> ${i + 1}. ${v}`).join("\n")}`
+    warnLists.push(str)
+  }
+  if (warnLists.length < 1) throw "Tidak ada pengguna yang terkena peringatan di group ini!"
+  m.reply(`${monospace("==> [ W A R N  L I S T ] <==")}\n\n${warnLists.join("\n\n")}\n\n` + wm)
 }
-
-handler.help = ['Cekwarn @user']
+handler.help = ['listwarn']
 handler.tags = ['group']
-handler.command = /^cekwarn$/i
-
-handler.group = true
+handler.command = /^list(warn|warning)?$/i
 
 module.exports = handler
+
+const monospace = (string) => {
+  return '```' + string + '```'
+}
