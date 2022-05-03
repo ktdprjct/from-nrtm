de61aed01d123f5d189babdbdb86b3d198c3e595
ktdprjct <97012512+ktdprjct@users.noreply.github.com>
2022-05-03 23:15:41 +0800

    update warning.js

--- plugins/warning.js
+++ plugins/warning.js
@@ -1,29 +1,33 @@
-let handler = async (m, { conn, groupMetadata }) => {
-    let mention = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted ? m.quoted.sender : false
-    if (!mention) throw `Tag/mention orangnya!`
-    let warning = global.db.data.users[mention].warning
-    if (warning < 2) {
-        global.db.data.users[mention].warning += 1
-        m.reply(`⚠️ *WARNING +1*`)
-        m.reply('Kamu mendapatkan warn dari admin, total warn kamu sekarang *' + (warning + 1) + '* warn, Jika kamu mendapat warn *3 kali*, kamu akan dikeluarkan dari grup', mention)
-    } else if (warn == 2) {
-        global.db.data.users[mention].warning = 0
-        m.reply('Selamat tinggal')
-        await time(5000)
-        await conn.groupRemove(m.chat, [mention])
-        m.reply(`Kamu dikeluarkan dari group ${groupMetadata.subject} karena telah mendapat 3 kali warn`, mention)
-    }
-}
-handler.help = ['warn [@user]']
-handler.tags = ['group']
-handler.command = /^warn$/i
+// from rthelolchex
 
-handler.group = true
-handler.admin = true
-handler.botAdmin = true
+let handler = async(m, { conn, isGroup, isAdmin, isBotAdmin, usedprefix, args, participants }) =>{
+  if (!m.isGroup) return m.reply("Perintah ini hanya bisa digunakan di group!")
+  if (!isAdmin) return m.reply("Perintah ini hanya dapat dilakukan oleh admin!")
+  if (!isBotAdmin) return m.reply("Jadikan bot sebagai admin untuk menggunakan perintah ini!")
+  if (m.mentionedJid.length < 1) return m.reply("Tag user yang ingin di beri warning!")
+  participants.forEach(user => {
+    if (user.jid == m.mentionedJid[0] && user.isAdmin) return m.reply("Sadar bang, yang lu warn admin!")
+  })
+  let reasonText = args.splice(1).join` `
+  if (reasonText.length == 0) reasonText = "Nothing."
+  for (let userWarn of m.mentionedJid) {
+    let warnUsers = global.db.data.warns[m.chat].users[userWarn]
+    if (typeof warnUsers !== "object") global.db.data.warns[m.chat].users[userWarn] = {
+      count: 0,
+      reason: []
+    }
+    global.db.data.warns[m.chat].users[userWarn].reason.push(reasonText)
+    global.db.data.warns[m.chat].users[userWarn].count += 1
+    if (global.db.data.warns[m.chat].users[userWarn].count == 5) {
+      m.reply(`Baiklah, ` + "@" + userWarn.split`@`[0] + ` telah melebihi batas peringatan, anda akan dikick!.\n\nAlasan : \n` + global.db.data.warns[m.chat].users[userWarn].reason )
+      delete global.db.data.warns[m.chat].users[userWarn]
+      await conn.groupRemove(m.chat, [userWarn])
+    }
+    conn.sendButton(m.chat, "Pengguna " + "@" + userWarn.split`@`[0] + " memiliki " + global.db.data.warns[m.chat].users[userWarn].count +"/5 peringatan. hati-hati!\nAlasan : " + reasonText , wm, `Hapus peringatan (admin only)`, "${usedprefix}deletewarn", m)
+  }
+}
+handler.help = ['warn @mention']
+handler.tags = ['group']
+handler.command = /^warn(user)?$/i
 
 module.exports = handler
-
-const time = async (ms) => {
-    return new Promise(resolve => setTimeout(resolve, ms))
-}
