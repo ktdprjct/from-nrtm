f9ca4391c509551394d36177c6b382db0e5fdddf
ktdprjct <97012512+ktdprjct@users.noreply.github.com>
2022-05-05 07:48:40 +0800

    update tagall.js

--- plugins/tagall.js
+++ plugins/tagall.js
@@ -1,12 +1,27 @@
-let handler = async (m, { conn, text, participants }) => {
-  let users = participants.map(u => u.jid)
-  m.reply(text + '\n' + users.map(v => '@' + v.replace(/@.+/, '')).join`\n`, null, {
-    contextInfo: { mentionedJid: users }
-  })
-}
-handler.command = ['tagall']
+//let { getGroupMembers } = require('../lib/functions')
+let { MessageType, Presence } = require('@adiwajshing/baileys')
 
-handler.admin = true
+let handler = async(m, { conn, isGroup, isAdmin, participants, groupMetadata }) => {
+
+  await conn.updatePresence(m.chat, Presence.composing)
+  
+ let grup = await groupMetadata.participants
+ let member = await getGroupMembers(grup)
+
+    conn.reply(m.chat, `┏━━°❀❬ *LIST MEMBER* ❭❀°━━┓\n┃\n${member.map(v => '┃• @' + v.replace(/@.+/, '')).join('\n')}\n┗━━━━━━━━━━━━━━`, null, { contextInfo: { mentionedJid: member } })               
+}
+handler.help = ['listmember','tagall','tagmember']
+handler.tags = ['group']
+handler.command = /^(listmember|tagall|tagmember)$/i
 handler.group = true
+handler.admin = true
 
 module.exports = handler
+
+async function getGroupMembers(participants) {
+  members = []
+  participants.forEach(user => {
+    if (!user.isAdmin) members.push(user.jid)
+  })
+return members
+}
